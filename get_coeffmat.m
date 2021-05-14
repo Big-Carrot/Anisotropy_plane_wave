074ea3573e48be12bfab39aa885edf80dc3d258e
UH-YSJ <56280920+Big-Carrot@users.noreply.github.com>
2021-05-14 12:35:31 -0500

    Bug fixing

--- get_coeffmat.m
+++ get_coeffmat.m
@@ -6,7 +6,7 @@ B2 = zeros(3,3);
 
 for i = 1:3
     for j = 1:3
-   B0(i,j) = c(i,1,j,i)*k1^2+c(i,1,j,2)*k1*k2+c(i,2,j,1)*k2*k1+c(i,2,j,2)*k2^2;
+   B0(i,j) = c(i,1,j,1)*k1^2+c(i,1,j,2)*k1*k2+c(i,2,j,1)*k2*k1+c(i,2,j,2)*k2^2;
    B1(i,j) = c(i,1,j,3)*k1+c(i,2,j,3)*k2+c(i,3,j,2)*k2+c(i,3,j,1)*k1;
    B2(i,j) = c(i,3,j,3);
     end
